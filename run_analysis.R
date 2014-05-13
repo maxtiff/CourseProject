a79e3416e1239060737608043eea20bbfa215629
Travis <travismay@gmail.com>
2014-05-13 15:59:20 -0500

    Modify run_analysis.R documentation

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,14 @@
 ## Source data.table library to efficiently process large data sets 
 library(data.table)
 
-## Setting up data variables
+## Setting up data variables for download.
 dataDirPath <- "data"
 datasetDirPath <- file.path(dataDirPath, "UCI HAR Dataset")
 zipFilename <- "UCI HAR Dataset.zip"
 zipPath <- file.path(dataDirPath, zipFilename)
 zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
-## Downloading/Unzipping data
+## Downloading/Unzipping data iff files do not previously exist.
 if(!file.exists(dataDirPath)) { 
   dir.create(dataDirPath) 
 }
@@ -21,11 +21,11 @@ if(!file.exists(datasetDirPath)) {
   unzip(zipPath, exdir=dataDirPath)
 }
 
+print(datasetDirPath)
 
+#### Function to load and manage data files.
 
-## Instructions
-#You should create one R script called run_analysis.R that does the following:
-#
+#### Proceed to assignment requirements.
 
 # 1. Merges the training and the test sets to create one data set.
 
