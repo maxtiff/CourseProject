8cd65701c4d6964a0642d42c3c443cc436853b05
Travis <travismay@gmail.com>
2014-05-13 11:45:14 -0500

    Modify run_analysis.R with entirely new structure

--- run_analysis.R
+++ run_analysis.R
@@ -1,46 +1,40 @@
 ## Source data.table library to efficiently process large data sets 
 library(data.table)
 
-#### Function organizes all files and then merges the test and train data sets together. Necessary files
-merge <- function(mainDir, testDir, trainDir) {
-  #Place
-  
-}
-
-
-#### Function downloads dataset. Data will not be downloaded if it previously exists.
-download <- function(url, dataFilename, zipFilename) {
-  dataDirPath <- "data"
-  datasetDirPath <- file.path(dataDirPath, dataFilename)
-  zipPath <- file.path(dataDirPath, zipFilename)
-  
-  ## Downloading/Unzipping data
-  if(!file.exists(dataDirPath)){ 
-    dir.create(dataDirPath) 
-  }
-  
-  if(!file.exists(zipPath)){ 
-    download.file(url, zipPath, mode="wb")
-  }
-  
-  if(!file.exists(datasetDirPath)) { 
-    unzip(zipPath, exdir=dataDirPath)
-  }
-  
-  return(datasetDirPath)
-  
-}
-
-## Setting up variables that are used as parameters in the download function.
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-dataFilename <- "UCI HAR Dataset"
+## Setting up data variables
+dataDirPath <- "data"
+datasetDirPath <- file.path(dataDirPath, "UCI HAR Dataset")
 zipFilename <- "UCI HAR Dataset.zip"
+zipPath <- file.path(dataDirPath, zipFilename)
+zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
-## Download file and return dir that contains the data.
-mainDir <- download(url, dataFilename, zipFilename)
+## Downloading/Unzipping data
+if(!file.exists(dataDirPath)) { 
+  dir.create(dataDirPath) 
+}
 
-scan
+if(!file.exists(zipPath)) {
+  download.file(zipUrl, zipPath, mode="wb") 
+}
 
-list.dirs()
+if(!file.exists(datasetDirPath)) {
+  unzip(zipPath, exdir=dataDirPath)
+}
+
+
+
+## Instructions
+#You should create one R script called run_analysis.R that does the following:
+#
+
+# 1. Merges the training and the test sets to create one data set.
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
+# 3. Uses descriptive activity names to name the activities in the data set.
+
+# 4. Appropriately labels the data set with descriptive activity names.
+
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 
