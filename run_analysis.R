2d6c113b16164fce4e38051ef445ed51cd3af7b5
Travis <travismay@gmail.com>
2014-05-13 11:05:40 -0500

    Modify run_analysis.R with skeletal merge function

--- run_analysis.R
+++ run_analysis.R
@@ -4,29 +4,34 @@ library(data.table)
 #### Function organizes all files and then merges the test and train data sets together. Necessary files
 merge <- function(mainDir, testDir, trainDir) {
   
+  
 }
 
-## Setting up data variables
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-dataDirPath <- "data"
-datasetDirPath <- file.path(dataDirPath, "UCI HAR Dataset")
+
+#### Function downloads dataset. Data will not be downloaded if it previously exists.
+download <- function(url, dataFilename, zipFilename)
+  dataDirPath <- "data"
+  datasetDirPath <- file.path(dataDirPath, dataFilename)
+  zipPath <- file.path(dataDirPath, zipFilename)
+  
+  ## Downloading/Unzipping data
+  if(!file.exists(dataDirPath)){ 
+    dir.create(dataDirPath) 
+  }
+  
+  if(!file.exists(zipPath)){ 
+    download.file(fileUrl, zipPath, mode="wb")
+  }
+  
+  if(!file.exists(datasetDirPath)) { 
+    unzip(zipPath, exdir=dataDirPath)
+    unlink(zipPath)
+  }
+
+
+## Setting up variables that are used as parameters in the download function.
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 zipFilename <- "UCI HAR Dataset.zip"
-zipPath <- file.path(dataDirPath, zipFilename)
-
-
-## Downloading/Unzipping data
-if(!file.exists(dataDirPath)){ 
-  dir.create(dataDirPath) 
-}
-
-if(!file.exists(zipPath)){ 
-  download.file(fileUrl, zipPath, mode="wb")
-}
-
-if(!file.exists(datasetDirPath)) { 
-  unzip(zipPath, exdir=dataDirPath)
-  unlink(zipPath)
-}
-
+dataFilename <- "UCI HAR Dataset"
 
 
