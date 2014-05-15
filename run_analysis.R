1013515fdc4ece0c5ccbfbbbc58bd5fcb06518d6
Travis <travismay@gmail.com>
2014-05-15 13:01:10 -0500

    Modify run_analysis.R to load numerous data files

--- run_analysis.R
+++ run_analysis.R
@@ -1,40 +1,85 @@
 ## Source data.table library to efficiently process large data sets 
 library(data.table)
 
-## Setting up data variables for download.
+
+## Set up path variables for file download.
 dataDirPath <- "data"
 datasetDirPath <- file.path(dataDirPath, "UCI HAR Dataset")
 zipFilename <- "UCI HAR Dataset.zip"
 zipPath <- file.path(dataDirPath, zipFilename)
 zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
+
 ## Downloading/Unzipping data iff files do not previously exist.
 if(!file.exists(dataDirPath)) { 
+  
   dir.create(dataDirPath) 
+  
 }
 
 if(!file.exists(zipPath)) {
+  
   download.file(zipUrl, zipPath, mode="wb") 
+  
 }
 
 if(!file.exists(datasetDirPath)) {
+  
   unzip(zipPath, exdir=dataDirPath)
+  
 }
 
-print(datasetDirPath)
 
-#### Function to load and manage data files.
+####Load and manage features and activity data files.
+
+## load features label data. 
+featuresFilePath <- file.path(datasetDirPath, "features.txt")
+featuresNamesFile <- read.table(featuresFilePath)
+names(featuresNamesFile) <- c('feature id', 'feature name')
+
+## load activity labels data.
+activityLabelsFilePath <- file.path(datasetDirPath, "activity_labels.txt")
+activityFile <- read.table(activityLabelsFilePath)
+names(activityFile) <- c('activity id', 'activity name')
+
+
+#### Function loads user-defined raw set. Either 'test' or 'train' data set.
+dataSetLoader <- function(set) {
+  
+  ## Set file path variables for subjects, X (observations) and Y (activity) text files.
+  outputPath <- file.path(datasetDirPath,set,paste("X_",set,".txt", sep=""))
+  activityPath <- file.path(datasetDirPath,set,paste("Y_",set,".txt", sep=""))
+  subjectPath <- file.path(datasetDirPath,set,paste("subject_",set,".txt",sep=""))
+  
+  ## Read in data from path variables.
+  outputData <- read.table(outputPath)
+  activityData <- read.table(activityPath)
+  subjectPath <- read.table(subjectPath)
+  
+  
+  
+  
+}
+
+print(dataSetLoader('test'))
 
 #### Proceed to assignment requirements.
 
-# 1. Merges the training and the test sets to create one data set.
+## Merges the training and the test sets to create one data set.
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 
-# 3. Uses descriptive activity names to name the activities in the data set.
+## Extracts only the measurements on the mean and standard deviation for each measurement.
 
-# 4. Appropriately labels the data set with descriptive activity names.
 
-# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## Use descriptive activity names to name the activities in the data set.
+## Load activity labels data.
+activityLabelsFilePath <- file.path(datasetDirPath, "activity_labels.txt")
+activityFile <- read.table(activityLabelsFilePath)
+names(activityFile) <- c('activity id', 'activity name')
+
+## Appropriately labels the data set with descriptive activity names.
+activityData <- cbind(activityData,activityFile)
+
+## Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 
