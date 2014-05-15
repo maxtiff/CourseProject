df8fd9e70c41e86cf0ac6c9b46d57574c271ecd3
Travis <travismay@gmail.com>
2014-05-15 16:14:30 -0500

    Finish Getting and Cleaning Data code

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
-## Source data.table library to efficiently process large data sets 
+## Source data.table library to efficiently process large data sets and reshape2 library to melt merged data set.
 library(data.table)
+library(reshape2)
 
 
 ## Set up path variables for file download.
@@ -12,21 +13,15 @@ zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20
 
 ## Downloading/Unzipping data iff files do not previously exist.
 if(!file.exists(dataDirPath)) { 
-  
   dir.create(dataDirPath) 
-  
 }
 
 if(!file.exists(zipPath)) {
-  
   download.file(zipUrl, zipPath, mode="wb") 
-  
 }
 
 if(!file.exists(datasetDirPath)) {
-  
   unzip(zipPath, exdir=dataDirPath)
-  
 }
 
 
@@ -35,18 +30,19 @@ if(!file.exists(datasetDirPath)) {
 ## Load features label data. 
 featuresFilePath <- file.path(datasetDirPath, "features.txt")
 featuresNames <- read.table(featuresFilePath)
-names(featuresNames) <- c('featureid', 'featurename')
+names(featuresNames) <- c("id", "feature")
 
 ## Use descriptive activity names to name the activities in the data set.
 ## Load activity labels data.
 activityLabelsFilePath <- file.path(datasetDirPath, "activity_labels.txt")
 activityLabels <- read.table(activityLabelsFilePath)
-names(activityLabels) <- c('activityid', 'activityname')
+names(activityLabels) <- c("id", "activity")
 
 
-#### Function loads user-defined raw set. Either 'test' or 'train' data set.
-dataSetLoader <- function(set) {
-  
+#### Function loads user-defined raw set and creates a data table from all appropriate files. Either 'test' or 'train', representing 
+#### observations drawn from the different experiment groups, can be used in the argument.
+dataReader <- function(set) {
+
   ## Set file path variables for subjects, X (observations) and Y (activity) text files.
   outputPath <- file.path(datasetDirPath,set,paste("X_",set,".txt", sep=""))
   activityPath <- file.path(datasetDirPath,set,paste("Y_",set,".txt", sep=""))
@@ -58,27 +54,33 @@ dataSetLoader <- function(set) {
   subjectData <- read.table(subjectPath)
   
   ## Appropriately labels the data set with descriptive activity names.
-  ## Use factor() to map activity ids to labels
-  outputData <- cbind(outputData,factor(activityData[[1]],levels=activityLabels$activityid,labels=activityLabels$activityname))
+  outputData <- cbind(outputData,factor(activityData[[1]],levels=activityLabels$id,labels=activityLabels$activity))
 
   ## Add subject ids to outputData data frame
   outputData <-cbind(outputData,factor(subjectData[[1]]))
   
-  names(outputData) <- c(as.character(featuresNames$featurename),"activity", "subject")
+  ## Add header information to data set
+  names(outputData) <- c(as.character(featuresNames$feature),"activity", "subject")
+  
+  ## Extracts only the measurements on the mean and standard deviation for each measurement.
+  outputData <- outputData[grep(pattern="(mean\\(\\)|std\\(\\))|^(activity|subject)$",names(outputData),ignore.case=T)]
   
   return(outputData)
+  
 }
 
-testData <- dataSetLoader('test')
-trainData <- dataSetLoader('train')
 
+## Create two data sets with 'test' and 'train' data. 
+testData <- dataReader("test")
+trainData <- dataReader("train")
 
 ## Merge the training and the test sets to create one data set.
 mergedData <- rbind(trainData, testData)
 
-## Extracts only the measurements on the mean and standard deviation for each measurement.
+## Melt data to long $ tall form
+meltedData <- melt(mergedData,id=c("subject","activity"))
 
+## Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidyData <- dcast(meltedData,subject+activity~variable,mean)
 
-## Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-
+write.table(tidyData,"tidy_data.txt")
